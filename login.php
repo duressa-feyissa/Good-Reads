f2714b50282ea120f2ba3779fc8fe00ab338dda4
duressa-feyissa <duresa.feyisa.gose@gmail.com>
2024-06-24 13:48:47 +0300

    :sparkles: Authentication and Autherization

---
+++ login.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <!--====== Required meta tags ======-->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!--====== Title ======-->
+    <title>Login - Yenibab Mead</title>
+    <link rel="shortcut icon" href="images/favicon.png" type="image/png">
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/jquery.nice-number.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/default.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+    <style>
+        html, body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+        .developer-credit {
+            text-align: center;
+            font-size: 18px;
+            margin-top: 20px;
+            color: #ffffff;
+        }
+        .developer-credit span {
+            font-weight: bold;
+            color: #f0f0f0;
+        }
+    </style>
+</head>
+
+<body>
+    <section id="count-down-part" class="bg_cover pt-70 pb-120" data-overlay="4" style="background-image: url(assets/images/login-bg.jpg); height: 100vh;">
+        <div class="container">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-lg-6">
+                    <div class="count-down-cont pt-50">
+                        <h3>Welcome Back</h3>
+                        <h2>እንኳን ወደ ጥሩ ንባብ ገፅ በደህና መጡ!</h2>
+                    </div>
+                    <div class="developer-credit">
+                        Developed by  <span>Duresa Feyisa</span> , <span>Gemechis Elias</span>, <span>Habteyesus Tadese</span>, <span>Etsub Hayelom</span> and <span>Fikre</span>.
+                    </div>
+                </div>
+                <div class="col-lg-5 offset-lg-1 col-md-8">
+                    <div class="category-form category-form-3 pt-50">
+                        <div class="form-title text-center">
+                            <h3>New Registration</h3>
+                            <span>Login now </span><br>
+                           
+                        </div>
+                        <div class="main-form">
+                            <form action="login.php" method="POST">
+                                <div class="singel-form">
+                                    <input type="email" name="email" placeholder="Email">
+                                </div>
+                                <div class="singel-form">
+                                    <input type="password" name="password" placeholder="Password">
+                                </div>
+                                <div class="singel-form">
+                                    <button class="main-btn" name="submit" type="submit">Login</button>
+                                </div>
+                            </form>
+                            <p style="margin-top: 10px;">Not registered? <a href="signup.php" style="color: blue;">Sign Up Here</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/slick.min.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/jquery.nice-select.min.js"></script>
+    <script src="assets/js/jquery.nice-number.min.js"></script>
+    <script src="assets/js/jquery.countdown.min.js"></script>
+    <script src="assets/js/validator.min.js"></script>
+    <script src="assets/js/ajax-contact.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ signup.php
@@ -0,0 +1,128 @@
+<?php
+require_once 'user/user.php';
+require_once 'connection/connection.php';
+
+// Include connection.php and establish database connection
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Validate and sanitize input data
+
+    $user = new User($connection);
+    $result = $user->signup($name, $email, $password, "no_profile.png");
+
+    if ($result) {
+        echo "<script>alert('User created successfully!');</script>";
+        // Redirect to index.php or any other page
+        header("Location: login.php");
+        exit();
+    } else {
+        echo "<script>alert('Invalid credentials. User creation failed.');</script>";
+    }
+}
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Sign Up - Yenibab Mead</title>
+    <link rel="shortcut icon" href="images/favicon.png" type="image/png">
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/jquery.nice-number.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/default.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+    <style>
+        html, body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+        .developer-credit {
+            text-align: center;
+            font-size: 18px;
+            margin-top: 20px;
+            color: #ffffff;
+        }
+        .developer-credit span {
+            font-weight: bold;
+            color: #f0f0f0;
+        }
+    </style>
+</head>
+
+<body>
+
+    <section id="count-down-part" class="bg_cover pt-70 pb-120" data-overlay="4" style="background-image: url(assets/images/login-bg.jpg); height: 100vh;">
+        <div class="container">
+            <div class="row align-items-center justify-content-center">
+                <div class="col-lg-6">
+                    <div class="count-down-cont pt-50">
+                        <h3>Welcome Back</h3>
+                        <h2>እንኳን ወደ ጥሩ ንባብ ገፅ በደህና መጡ!</h2>
+                    </div>
+                    <div class="developer-credit">
+                        Developed by <span>Duresa Feyisa</span> , <span>Gemechis Elias</span>, <span>Habteyesus Tadese</span>, <span>Etsub Hayelom</span> and <span>Fikre</span>.
+                    </div>
+                </div>
+                <div class="col-lg-5 offset-lg-1 col-md-8">
+                    <div class="category-form category-form-3 pt-50">
+                        <div class="form-title text-center">
+                            <h3>New Registration</h3>
+                            <span>Sign up now </span>
+                        </div>
+                        <div class="main-form">
+                            <form action="signup.php" method="POST">
+                                <div class="singel-form">
+                                    <input type="text" name="name" placeholder="Your name">
+                                </div>
+                                <div class="singel-form">
+                                    <input type="email" name="email" placeholder="Your Email">
+                                </div>
+                                <div class="singel-form">
+                                    <input type="password" name="password" placeholder="Password">
+                                </div>
+                                <div class="singel-form">
+                                    <button class="main-btn" name="submit" type="submit">Sign Up</button>
+                                </div>
+                            </form>
+
+                            <p style="margin-top: 10px;">Already registered? <a href="login.php" style="color: blue;">Login Here</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/slick.min.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/jquery.nice-select.min.js"></script>
+    <script src="assets/js/jquery.nice-number.min.js"></script>
+    <script src="assets/js/jquery.countdown.min.js"></script>
+    <script src="assets/js/validator.min.js"></script>
+    <script src="assets/js/ajax-contact.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
